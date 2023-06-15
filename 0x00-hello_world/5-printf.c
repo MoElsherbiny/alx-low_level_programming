0539e9e1f893cbd103043bc34767b03fe83b7f01
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-06-16 02:13:52 +0300

    Creating a betty coding styling

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/*
+1. Start
+2. Include the standard input/output library (stdio.h)
+3. Define the main function
+4. Print the message "with proper grammar, but the outcome is a piece of art," to the console using the printf function
+5. Return 0 to indicate successful termination of the program
+6. End
+*/
 int main()
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
