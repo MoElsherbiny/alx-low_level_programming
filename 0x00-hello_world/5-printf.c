00525266ba168536a3cd4a60a9cc95e8c650013b
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-06-16 02:18:08 +0300

    Creating a betty coding styling

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
-1. Start
-2. Include the standard input/output library (stdio.h)
-3. Define the main function
-4. Print the message "with proper grammar, but the outcome is a piece of art," to the console using the printf function
-5. Return 0 to indicate successful termination of the program
-6. End
+/**
+*1. Start
+*2. Include the standard input/output library (stdio.h)
+*3. Define the main function
+*4. Print the message "with proper grammar, but the outcome is a piece of art," to the console using the printf function
+*5. Return 0 to indicate successful termination of the program
+*6. End
 */
 int main(void)
 {
