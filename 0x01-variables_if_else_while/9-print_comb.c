08d25a22d8a137f6cd0bcbbc7405d5a4c0a0e01b
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-06-17 00:31:33 +0300

    Challenge

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,17 +4,36 @@
  *
  * Return: Always(Success)
  */
-int main(void)
+int main()
 {
-int i;
+int i, j, k;
+
 for (i = 0; i < 10; i++)
 {
 putchar(i + '0');
 if (i != 9)
 {
-putchar(',');
-putchar(' ');
+putchar(', ');
+}
+for (j = i + 1; j < 10; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (j != 9)
+{
+ putchar(', ');
+}
+for (k = j + 1; k < 10; k++)
+{
+ putchar(j + '0');
+ putchar(k + '0');
+ if (k != 9)
+ {
+ putchar(', ');
+ }
 }
 }
-putchar('\n');
-return (0) 
+}
+
+return 0;
+}
