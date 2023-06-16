ba56b7d6174c58218296b2497d64bb3fd725e835
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-06-17 01:22:28 +0300

    Advanced challenge

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,31 +2,23 @@
 /**
  * main - Entry point
  *
- * Return: Always(Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 int i, j, k;
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j < 10; j++)
-{
-for (k = 0; k < 10; k++)
-{
-if (i != j && i != k && j != k)
-{
+for (i = 0; i < 8; i++) {
+for (j = i + 1; j < 9; j++) {
+for (k = j + 1; k < 10; k++) {
 putchar(i + '0');
 putchar(j + '0');
 putchar(k + '0');
-if (i != 7 || j != 8 || k != 9)
-{
+if (i != 7 || j != 8 || k != 9) {
 putchar(',');
 putchar(' ');
 }
 }
 }
 }
-}
-putchar('\n');
 return (0);
 }
