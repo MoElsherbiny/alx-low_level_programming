fd9e9187eb5335e04e8a99e47cada7f4d2ac4c5d
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-06-17 01:35:00 +0300

    Advanced challenge

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,22 +8,24 @@ int main(void)
 {
 int i, j;
 
-for (i = 0; i < 100; i++) {
-for (j = i; j < 100; j++) {
-if (i == j) {
-continue;
-}
+for (i = 0; i <= 98; i++)
+{
+for (j = i + 1; j <= 99; j++)
+{
 putchar(i / 10 + '0');
 putchar(i % 10 + '0');
 putchar(' ');
 putchar(j / 10 + '0');
 putchar(j % 10 + '0');
-if (i != 99 || j != 98) {
+
+if (i != 98 || j != 99)
+{
 putchar(',');
 putchar(' ');
 }
 }
 }
+putchar('\n');
 
 return (0);
 }
