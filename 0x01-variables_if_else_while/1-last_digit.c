ddef20a0e65422106d03a480c7ba5034152dcc97
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-06-17 00:02:37 +0300

    This program first initializes the random number generator using the srand function with the current time as the seed. Then, it generates a random number using the rand function and stores it in the variable n. The last digit of n is calculated using the modulus operator (%) and stored in the variable last_digit. The program then uses an if-else statement to check the value of last_digit and prints the appropriate message using the printf function.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,22 +10,16 @@
 int main(void)
 {
 int n;
-int ld;
-
+int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-ld = n % 10;
-if (ld > 5)
-{
-printf("Last digit of %d is %d and isgreater than 5\n", n, ld);
-}
-else if (ld == 0)
-{
-printf("Last digit of %d is %d and is 0\n", n, ld);
-}
-else 
-{ 
-printf("Last digit of %d is %d and less than 6 and not 0\n", n, ld);
-}
+last_digit = n % 10;
+printf("Last digit of %d is %d ", n, last_digit);
+if (last_digit > 5)
+printf("and is greater than 5\n");
+else if (last_digit == 0)
+printf("and is 0\n");
+else
+printf("and is less than 6 and not 0\n");
 return (0);
 }
