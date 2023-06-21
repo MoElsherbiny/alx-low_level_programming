aa88d9527d6ab6b7b3ae2d7096aa8dcb10c81f66
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-06-21 05:01:58 +0300

    Betty coding style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,18 @@
 #include "main.h"
+
 /**
- * print_alphabet - Entry point
- * Return:0
+ * print_alphabet - prints the lowercase alphabet, followed by a new line
+ *
+ * Return: void
  */
 void print_alphabet(void)
 {
-char c = 'a';
+    char c = 'a';
 
-while (c <= 'z')
-{
-
-_putchar(c);
-
-c++;
-
-}
-_putchar('\n');
+    while (c <= 'z')
+    {
+        _putchar(c);
+        c++;
+    }
+    _putchar('\n');
 }
