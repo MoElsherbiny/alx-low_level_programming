3cfbccf6db808dfd0ff60c5e56f67bed5144ef07
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-06-30 02:33:05 +0300

    Challenge-Advanced-5

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -21,4 +21,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 void print_number(int n);
 int _putchar(char c);
+void print_buffer(char *b, int size);
 #endif /*MAIN_H*/
