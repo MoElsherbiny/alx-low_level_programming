c288d0d0fbd69d57a691195a4f9d3726b909b1cb
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-06-30 01:07:02 +0300

    Challenge-5

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,4 +10,5 @@ void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
 #endif /*MAIN_H*/
