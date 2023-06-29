5d4eb5b9b498d36e65a58ef567516ddf852eb21b
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-06-30 02:17:09 +0300

    Challenge-7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,28 @@
 #include "main.h"
-/**
- * leet - encodes a string into 1337
- * @str: the string to encode
- * Return: returns the encoded string
- */
-char *leet(char *str)
-{
-    int i, j;
-    char letters[] = "aAeEoOtTlL";
-    char leet[] = "4433007711";
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        for (j = 0; letters[j] != '\0'; j++)
-        {
-            if (str[i] == letters[j])
-            {
-                str[i] = leet[j];
-                break;
-            }
-        }
-    }
-    return str;
+/**
+ * leet - encodes a string in 1337
+ * @s: string to be encoded
+ *
+ * Return: the resulting string;
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+
+	return (s);
 }
