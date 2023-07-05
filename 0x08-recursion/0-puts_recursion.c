09fdf5ea54690fbe0eea3a8e0324d295b77877ed
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-07-06 00:09:05 +0300

    Recursion-challenge-0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,19 +9,10 @@
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
-return;
-putchar(*s);
-_puts_recursion(s + 1);
-}
-
-/**
- * main - entry point of the program
- *
- * Return: 0 on success
- */
-int main(void)
 {
-char *s = "Puts with recursion\n";
-_puts_recursion(s);
-return (0);
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
 }
