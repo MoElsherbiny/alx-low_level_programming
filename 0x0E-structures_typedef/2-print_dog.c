381ea5e385c030dfb21d69684e725097ec7ce06b
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-07-17 23:23:14 +0300

    structures_typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,7 @@ void print_dog(struct dog *d)
 {
 if (d == NULL) /* check if d is valid */
 return;
-printf("Name: %s\n", d->name ? d->name : "(nil)"); 
+printf("Name: %s\n", d->name ? d->name : "(nil)");
 printf("Age: %f\n", d->age); /* print age */
 printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
