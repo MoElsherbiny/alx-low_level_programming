5a3c58939afb7e2ec8589663ab049a611c65a2c7
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-07-20 01:08:25 +0300

    function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,24 +1,19 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - performs simple operations (+, -, *, /, %)
- *
- * Usage: calc num1 operator num2
- *
- * @argc: number of arguments passed to the program
- * @argv: array of arguments passed to the program
- *
- * Return: 0 on success,
- * or exit with status 98 if number of arguments is wrong,
- * or exit with status 99 if operator is none of the above,
- * or exit with status 100 if user tries to divide (/ or %) by 0.
- */
-int main(int argc, char **argv)
+* main - Prints the result of simple operations.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: Always 0.
+*/
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-
-int num1, num2, result;
-
-int (*op_func)(int, int);
+int num1, num2;
+char *op;
 
 if (argc != 4)
 {
@@ -27,25 +22,20 @@ exit(98);
 }
 
 num1 = atoi(argv[1]);
+op = argv[2];
 num2 = atoi(argv[3]);
 
-op_func = get_op_func(argv[2]);
-
-if (op_func == NULL)
+if (get_op_func(op) == NULL || op[1] != '\0')
 {
 printf("Error\n");
 exit(99);
 }
 
-if ((op_func == op_div || op_func == op_mod) && num2 == 0)
+if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 {
 printf("Error\n");
 exit(100);
 }
-
-result = op_func(num1, num2);
-
-printf("%d\n", result);
-
+printf("%d\n", get_op_func(op)(num1, num2));
 return (0);
 }
