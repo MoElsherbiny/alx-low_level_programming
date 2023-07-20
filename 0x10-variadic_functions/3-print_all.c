c21a9411b7ac2dc83626a7620c480942ccdcecfa
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-07-20 22:16:19 +0300

    variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,12 +1,9 @@
-#include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
 
 /**
  * print_all - prints anything
  * @format: list of types of arguments passed to the function
- *
- * Return: nothing
  */
 void print_all(const char * const format, ...)
 {
