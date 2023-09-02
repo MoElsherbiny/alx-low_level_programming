4b829f6abc8ee496792a0d717972c16ecf1ef6f9
Mohamed Elsherbiny <mohamedelsherbiny212@gmail.com>
2023-09-02 23:05:48 +0300

    0-strcat-o

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,31 +1,25 @@
 #include "main.h"
-/**
-*_strstr - The _strstr() function finds the first occurrence
-* of the substring needle in the string haystack.
-* The terminating null bytes (\0) are not compared
-*@haystack: string where the search is made
-*@needle: string whose occurence is searched in haystack
-*Return:Returns a pointer to the beginning of the located
-* substring, or NULL if the substring is not found.
-*/
 
-char *_strcat(char *haystack, char *needle)
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
 {
-int i;
-if (*needle == 0)
-return (haystack);
-while (*haystack)
-{
+int i, j;
 i = 0;
-if (haystack[i] == needle[i])
-{
-do {
-if (needle[i + 1] == '\0')
-return (haystack);
+j = 0;
+while (dest[i] != '\0')
 i++;
-} while (haystack[i] == needle[i]);
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
 }
-haystack++;
-}
-return ('\0');
+dest[i] = '\0';
+return (dest);
 }
